7ab3087bcb4c4e58a94353d3618b0ed83a824a93
Eduardo Patrocinio <epatro@gmail.com>
2018-04-23 14:15:27 -0400

    Document how to restore a single Cloudant DB

--- scripts/restoreCloudant.sh
+++ scripts/restoreCloudant.sh
@@ -1,12 +1,19 @@
 JOB=icp-cloudant-restore
+CONFIGMAP=cloudant-dbs
+
 DBS=$1
 
 echo Deleting job
 kubectl delete job $JOB
 
+echo Deleting config map
+kubectl delete configmap $CONFIGMAP
+
+echo Creating config map
+export DBNAME=$DBS
+kubectl create configmap $CONFIGMAP --from-literal=dbnames=$DBS 
+
 echo Creating job
-echo Databases: $DBS
-DBNAME=$DBS
 kubectl create -f ../resources/icp-cloudant-restore-job.yaml
 
 kubectl describe job $JOB
