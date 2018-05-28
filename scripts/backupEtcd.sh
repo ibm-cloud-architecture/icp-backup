ead8d5e1e5ad307805ddcce94ff76dd33e4d623b
Eduardo Patrocinio <epatro@gmail.com>
2018-05-28 14:46:59 -0300

    Add backup etcd

--- scripts/backupEtcd.sh
+++ scripts/backupEtcd.sh
@@ -8,4 +8,4 @@ echo "Current Time : $CURRENT_TIME"
 FILE_NAME="$BASE_FILE_NAME.$CURRENT_TIME.db"
  echo "Back up to the following file: " "$FILE_NAME"
 
-etcdctl3 snapshot save $FILE_NAME
+$etcdctl3 snapshot save $FILE_NAME
